6d7f9fcb1df21acb0a1eaea4a4dd37958c601a86
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2023-11-21 21:36:04 +0900

    fix(core): remove example

--- app_core/src/lib.rs
+++ app_core/src/lib.rs
@@ -1,18 +1,3 @@
-//! # Core
-//!
-//! ## Examples
-//!
-//! ```no_run
-//! use anyhow::Result;
-//! use core::add;
-//!
-//! #[tokio::main]
-//! async fn main() -> Result<()> {
-//!     add(1, 1).await?;
-//!     Ok(())
-//! }
-//! ```
-//!
 mod app;
 pub mod error;
 
